6f92ef366aaed35e705cf0df82824794b84a548e
jericjan <cantilfrederick@gmail.com>
2024-05-03 13:28:21 +0800

    stuff

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from "react-native";
+import { ImageBackground, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 export default function App() {
+  const [sound, setSound] = useState();
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(require("./laugh.mp3"));
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    playSound();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true}></StatusBar>
+      <ImageBackground
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        source={require("./cat.jpg")}
+        resizeMode="cover"
+      >
+        <TouchableOpacity
+          style={styles.full}
+          onPress={playSound}
+        ></TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  full: {
+    width: "100%",
+    height: "100%",
   },
 });
